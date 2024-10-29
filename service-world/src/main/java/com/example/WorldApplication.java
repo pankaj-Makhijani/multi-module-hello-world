5539518c4b9a8cd3c3ea5c23e8ee8af89f11e4fc
Pankaj Makhijani <pankaj.makhijani@rishabhsoft.com>
2024-10-29 16:51:08 +0530

    initial commit

---
+++ service-world/src/main/java/com/example/WorldApplication.java
@@ -0,0 +1,23 @@
+package com.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class WorldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WorldApplication.class, args);
+    }
+}
+
+@RestController
+class WorldController {
+    @GetMapping("/world")
+    public String world() {
+        return "Hello from Service World!";
+    }
+}
+
